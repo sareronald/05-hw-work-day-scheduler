16e14a34b690b3f01d25457598fae05013bccd97
Sarah Ronald <sareronald@hotmail.com>
2020-09-18 21:55:15 +1000

    refresh function to update screen and row colours

--- script.js
+++ script.js
@@ -1,50 +1,86 @@
 $(document).ready(function () {
     // display todays date and time at the top of the schedule planner
     var date = $("#todays-date");
-    var now = moment().format("MMMM Do YYYY");
-    var time = moment().format("HH:mm");
-    $(date).text(now + " // ");
-    $(date).append(time);
+
+    function refresh() {
+        var now = moment().format("MMMM Do YYYY");
+        var time = moment().format("HH:mm");
+        $(date).text(now + " // ");
+        $(date).append(time);
+        var h = moment().hour();
+
+
+        for (var row = 9; row <= 17; row++) {
+            var currentRow = $("#row" + row);
+            currentRow.removeClass(["past", "present", "future"])
+
+            if (row < h)
+                currentRow.addClass("past");
+            else if (row < h)
+                currentRow.addClass("future");
+            else if (row === h)
+                currentRow.addClass("present");
+        }
+
+        setTimeout(refresh, 1000);
+        console.log("setTimeout");
+    }
+    refresh();
 
     // refer back to calculator - amount per person - Unit 4 Activity 13
     // allow the user to add comments into their day planner textarea  
-    var nineEl = document.querySelector("#entry9am");
-    var tenEl = document.querySelector("#entry10am");
-    var elevenEl = document.querySelector("#entry11am");
-    var twelveEl = document.querySelector("#entry12am");
-    var oneEl = document.querySelector("#entry1pm");
-    var twoEl = document.querySelector("#entry2pm");
-    var threeEl = document.querySelector("#entry3pm");
-    var fourEl = document.querySelector("#entry4pm");
-    var fiveEl = document.querySelector("#entry5pm");
-    var saveBtn = document.querySelector(".save-button");
+    // var element9 = document.querySelector("#entry9");
+    // var element10 = document.querySelector("#entry10");
+    // var entryEl = document.querySelector(".entry");
+    // var saveBtn = document.querySelector(".save-button");
 
-    var thingsToDo = [];
-    // getting the value for 9am
-    function toDoList(event) {
-        event.preventDefault();
-        localStorage.setItem('entry9am', nineEl.value);
-        var nineToDo = nineEl.value;
-        thingsToDo.push({ nineToDo: nineToDo });
+    // var thingsToDo = [];
 
-    }
-    console.log(thingsToDo);
-   
-
-    // function saveThisToo {
-    // localStorage.setItem("something",JSON.stringify(something));
-    // }
-    // var savedToDo = JSON.parse(localStorage.getItem)
-    saveBtn.addEventListener("click", toDoList);
-    // console.log(saveBtn);
-
-    nineEl.innerHTML = localStorage.getItem('entry9am')
-
-    // click on the save button and save todo onto the list in local storage
-    //$(".save-button").on(click, function(){
-
-    // var entry9am = $("#entry9am");
-    // console.log(entry9am.val());
-    // each timeblock is colour coded to indicate past, present or future?
-
+    // // getting the value for 9am
+    // // function toDoList(event) {
+    //     event.preventDefault();
+    //     localStorage.setItem('entry9', element9.value);
+    //     var nineToDo = element9.value;
+    //     thingsToDo.push({ nineToDo: nineToDo });
+    // localStorage.setItem('entry10', tenEl.value);
+    // var tenToDo = tenEl.value;
+    // var elevenToDo = eleven.value;
+    // thingsToDo.push({elevenToDo: elevenToDo });
+    // thingsToDo.push({twelveToDo: twelveToDo });
 })
+// give give each corresponding button a data-time, then make an event listener like this?
+document.querySelector(".save-button").addEventListener("click", function (event) {
+    event.preventDefault();
+    let time = parseInt($(this).data("time"));
+    let entry = $(`.entry${time}`.val());
+    localStorage.setItem(`entry${i}`, entry)
+})
+
+
+
+    // // click on the save button and save todo onto the list in local storage
+    // saveBtn.addEventListener("click", toDoList);
+    // element9.innerHTML = localStorage.getItem('entry9')
+
+    // rows change colour as the time continues
+    // if HH = HH make it this colour blue and text white
+    // var hour = moment(HH) 
+    // hr = (new Date()).getHours();
+
+    // changing row colours according to past, present, future
+    //?is that the right ID?
+
+
+
+
+    // var hour = 10;
+    // if (hour < 10) {
+    //     document.getElementById("entry10").className = "past";
+    // }
+    // // else in the future light grey
+    // else if (hour > 10) {
+    //     document.getElementById("entry11").className = "future";
+    // }
+    // // else if it time is less than current time = white 
+    // else (hour === 10)
+    // document.getElementById("entry10").className = "present";
